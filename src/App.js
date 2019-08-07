f76120632b8b48343888a6fa34cb4c393bb2963d
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-08-07 07:12:58 +0100

    Add my favourite movie cover

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import mrRobotLogo from './mrRobot.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
-          <h1>Welcome to Coding starting in August 2019!!!</h1>
+      <img src={mrRobotLogo} alt='Mr. Robot is an American drama thriller television series created by Sam Esmail'/>
+          <h1>Welcome to Kodflix!</h1>
         
     </div>
   );
