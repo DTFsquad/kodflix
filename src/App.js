c598d3450fbc49796e745ba35c46107867eae748
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-08-07 12:57:57 +0100

    Add 6 TV titles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ function App() {
     <div className="App">
       <img src={mrRobotLogo} alt='Mr. Robot is an American drama thriller television series created by Sam Esmail'/>
           <h1>Welcome to Kodflix!</h1>
-        
+        <td>
+          <tr>
+            <li>Black Mirror</li>
+            <li>Breaking Bad</li>
+            <li>Deadh Note</li>
+          </tr>
+          <tr>
+            <li>Game of Thrones</li>
+            <li>The Walking Dead</li>
+            <li>The Wire</li>
+          </tr>
+        </td>
     </div>
   );
 }
