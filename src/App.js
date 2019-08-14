7112a2ea37c3e0998686649d3c0c516eb1f852d2
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-08-14 09:04:04 +0100

    Add the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -16,19 +16,34 @@ function App() {
         <div className='container'>
           <div className='item'>
             <img src={blackmirror} alt='Black Mirror movie cover'/>
+              <div className='overlay'>
+                <h1>Black Mirror</h1>
+              </div>
           </div>
           <div className='item'>
             <img src={breakingBad} alt='Breaking Bad movie cover'/></div>
+              <div className='overlay'>
+                <h1>Breaking Bad</h1>
+              </div>
           <div className='item'>
             <img src={deathNote} alt='Deadh Note movie cover'/></div>
         </div>  
         <div className='container'>
           <div className='item'>
             <img src={gameOfTrones} alt='Game of Thrones movie cover'/></div>
+              <div className='overlay'>
+                <h1>Games of Trones</h1>
+              </div>
           <div className='item'>
             <img src={walkingDead} alt='The Walking Dead movie cover'/></div>
+              <div className='overlay'>
+                <h1>Walking Dead</h1>
+              </div>
           <div className='item'>
             <img src={theWire} alt='The Wire movie cover'/></div>
+              <div className='overlay'>
+                <h1>The Wire</h1>
+              </div>
         </div>
 
     </div>
