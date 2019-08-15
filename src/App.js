aefc9c2ae890a44ba278c69b9b46309f768fbdcb
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-08-15 08:17:33 +0100

    Add the TV show title overlay on hover-over

--- src/App.js
+++ src/App.js
@@ -21,31 +21,39 @@ function App() {
               </div>
           </div>
           <div className='item'>
-            <img src={breakingBad} alt='Breaking Bad movie cover'/></div>
-              <div className='overlay'>
+            <img src={breakingBad} alt='Breaking Bad movie cover'/>
+            <div className='overlay'>
                 <h1>Breaking Bad</h1>
               </div>
+            </div>  
           <div className='item'>
-            <img src={deathNote} alt='Deadh Note movie cover'/></div>
+            <img src={deathNote} alt='Deadh Note movie cover'/>
+              <div className='overlay'>
+                <h1>Death Note</h1>
+              </div>
+          </div>
         </div>  
         <div className='container'>
           <div className='item'>
-            <img src={gameOfTrones} alt='Game of Thrones movie cover'/></div>
+            <img src={gameOfTrones} alt='Game of Thrones movie cover'/>
               <div className='overlay'>
                 <h1>Games of Trones</h1>
               </div>
+            </div>
           <div className='item'>
-            <img src={walkingDead} alt='The Walking Dead movie cover'/></div>
-              <div className='overlay'>
+            <img src={walkingDead} alt='The Walking Dead movie cover'/>
+            <div className='overlay'>
                 <h1>Walking Dead</h1>
-              </div>
+            </div>
+          </div>
           <div className='item'>
-            <img src={theWire} alt='The Wire movie cover'/></div>
-              <div className='overlay'>
+            <img src={theWire} alt='The Wire movie cover'/>
+            <div className='overlay'>
                 <h1>The Wire</h1>
-              </div>
+            </div>
+          </div>
+              
         </div>
-
     </div>
   );
 }
