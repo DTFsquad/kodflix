7b1b125efd20ab5f400f484097dd59693eed2b6b
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-08-20 12:40:39 +0100

    Introduce routing / navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Gallery from './Gallery.js';
+import Description from './Description';
 
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<br /> <br /> <br />
-				<Gallery />
-			</div>
+			<Router>
+				<div className="App">
+					<Route exact path="/" Component={Gallery} />
+					<Route path="/description" Component={Description} />
+					<Gallery />
+				</div>
+			</Router>
 		);
 	}
 }
+
 
 export default App;
---
+++ src/Description.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Description() {
+	return (
+		<div>
+			<h1>Welcome to description page, under construction!</h1>
+			<Link to="./">Back to home page</Link>
+		</div>
+	);
+}
