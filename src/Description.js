9ebfd23844db41b0ba603e72b5b36e26c279b332
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-08-24 18:10:13 +0100

    Introduce component state

--- src/Description.js
+++ src/Description.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Description() {
-	return (
-		<div>
-			<h1>Welcome to description page, under construction!</h1>
-			<Link to="./">Back to home page</Link>
-		</div>
-	);
+export default class Description extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			welcomeMessage: 'Welcome to description page, under construction!'
+		}
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				welcomeMessage: 'The best is yet to come!'
+			})
+		}, 3000);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>(this.state.welcomeMessage)</h1>
+				<Link to="./">Back to home page</Link>
+			</div>
+		);
+	}
 }
