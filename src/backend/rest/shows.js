885731d112c78b0f7d6b247600e8850289b78ec1
Florin DUMITRU <dumitruflorin19@yahoo.com>
2019-09-13 21:22:54 +0100

    Return a list of shows from the backend

---
+++ src/backend/rest/shows.js
@@ -0,0 +1,37 @@
+module.exports = function shows() {
+    return [
+        { 
+            id: 'blackMirror', 
+            title: 'Black Mirror', 
+            synopsis: 'Featuring stand-alone dramas -- sharp, suspenseful, satirical tales that explore techno-paranoia -- "Black Mirror" is a contemporary reworking of "The Twilight Zone" with stories that tap into the collective unease about the modern world.' 
+        },
+        { 
+            id: 'breakingBad', 
+            title: 'Breaking Bad', 
+            synopsis: 'Mild-mannered high school chemistry teacher Walter White thinks his life cannot get much worse. His salary barely makes ends meet, a situation not likely to improve once his pregnant wife gives birth, and their teenage son is battling cerebral palsy. But Walter is dumbstruck when he learns he has terminal cancer. Realizing that his illness probably will ruin his family financially, Walter makes a desperate bid to earn as much money as he can in the time he has left by turning an old RV into a meth lab on wheels.' 
+        },
+        { 
+            id: 'deathNote', 
+            title: 'Deadh Note',
+            synopsis: 'Death Note is a 2017 American neo-noir supernatural thriller film loosely adapted from the Japanese manga of the same name created by Tsugumi Ohba and Takeshi Obata. The film stars Nat Wolff, Lakeith Stanfield, Margaret Qualley, Shea Whigham, Paul Nakauchi, Jason Liles and Willem Dafoe, and follows the story of an American high school student named Light Turner, who finds a mysterious supernatural notebook known as the "Death Note", which can kill anyone whose name is written in its pages.' 
+        },
+        { 
+            id: 'gameOfTrones', 
+            title: 'Game of Thrones',
+            synopsis: 'George R.R. Martin best-selling book series "A Song of Ice and Fire" is brought to the screen as HBO sinks its considerable storytelling teeth into the medieval fantasy epic. It is the depiction of two powerful families -- kings and queens, knights and renegades, liars and honest men -- playing a deadly game for control of the Seven Kingdoms of Westeros, and to sit atop the Iron Throne. Martin is credited as a co-executive producer and one of the writers for the series, whose shooting locations include Northern Ireland, Malta, Croatia and Spain.' 
+        },
+        { 
+            id: 'walkingDead', 
+            title: 'The Walking Dead',
+            synopsis: 'Based on the comic book series written by Robert Kirkman, this gritty drama portrays life in the months and years that follow a zombie apocalypse. Led by former police officer Rick Grimes, his family and a group of other survivors find themselves constantly on the move in search of a safe and secure home. But the pressure each day to stay alive sends many in the group to the deepest depths of human cruelty, and they soon discover that the overwhelming fear of the survivors can be more deadly than the zombies walking among them.' 
+        },
+        { 
+            id: 'theWire', 
+            title: 'The Wire', 
+            synopsis: 'This series looks at the narcotics scene in Baltimore through the eyes of law enforcers as well as the drug dealers and users. Other facets of the city that are explored in the series are the government and bureaucracy, schools and the news media. The show was created by former police reporter David Simon, who also wrote many of the episodes.' 
+        }
+    ];
+}
+
+
+
